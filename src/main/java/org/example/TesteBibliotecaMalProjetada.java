77868488fe275d146a69025de3b429b565664e48
michellebritoo <dev.michellebrito@gmail.com>
2024-11-06 20:13:49 -0300

    Expert grasp pattern - base problem identention

--- src/main/java/org/example/TesteBibliotecaMalProjetada.java
+++ src/main/java/org/example/TesteBibliotecaMalProjetada.java
@@ -8,10 +8,18 @@ public class TesteBibliotecaMalProjetada {
         Livro livro1 = new Livro("O Senhor dos Anéis");
         Livro livro2 = new Livro("1984");
 
-        biblioteca.registrarEmprestimo(livro1, "João",
-                LocalDate.now().minusDays(10), LocalDate.now().minusDays(5));
-        biblioteca.registrarEmprestimo(livro2, "Maria",
-                LocalDate.now().minusDays(20), LocalDate.now().minusDays(15));
+        biblioteca.registrarEmprestimo(
+                livro1,
+                "João",
+                LocalDate.now().minusDays(10),
+                LocalDate.now().minusDays(5)
+        );
+        biblioteca.registrarEmprestimo(
+                livro2,
+                "Maria",
+                LocalDate.now().minusDays(20),
+                LocalDate.now().minusDays(15)
+        );
         System.out.println("Multas registradas:");
         biblioteca.exibirMultas();
     }
